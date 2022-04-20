f44191f3337156fa6687306c46a5c616d8f24d20
Johnson Wang <johnson.wang@northstartechlab.com>
2022-04-20 16:29:24 -0700

    added InMemoryUserDetail service for security. This is only used temporary for testing purpose.

---
+++ chap-05/taco-cloud/src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package sia.tacocloud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    List<UserDetails> usersList = new ArrayList<>();
+    usersList.add(new User("buzz", encoder.encode("password"),
+        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+    usersList.add(new User("woody", encoder.encode("password"),
+        Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+
+    return new InMemoryUserDetailsManager(usersList);
+  }
+
+}
